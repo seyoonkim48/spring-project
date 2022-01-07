3088d8fd936388b5aaacf721040f52e2700e1784
김세윤 <gimseyun@gimseyun-ui-MacBookPro.local>
2022-01-07 23:16:50 +0900

    초기 셋팅

---
+++ src/test/java/com/codestates/solo/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.solo.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
