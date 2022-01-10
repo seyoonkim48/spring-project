08229ea7086e0fd0177e3b4ad43747e88ac4be2a
gimseyun <seyoonkim48@gmail.com>
2022-01-10 23:59:26 +0900

    Chapter3 JPA

---
+++ src/main/java/com/codestates/solo/springproject/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.codestates.solo.springproject.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity //이건 JPA 어노테이션, 위의 둘은 롬복 어노테이션.
+public class Posts { // 실제 DB와 매칭될 클래스. JPA통해서 쿼리 날리는 경우, 실제 쿼리를 날리기보단 이 엔티티클래스 수정해서 작업~
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 이거 안써도 컬럼인거 아는데, 문자열 길이 늘리는 등의 옵션 적을때 사용함.
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false) // 타입이 VARCHAR가 아니라 TEXT임!
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/codestates/solo/springproject/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.codestates.solo.springproject.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
